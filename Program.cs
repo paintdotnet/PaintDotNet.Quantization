8394fb7fec0b0af558c9f3d4cc8d9a8536fd6205
Rick Brewster <rickbrew@users.noreply.github.com>
2021-04-05 19:28:51 -0700

    Initial check-in of blank project files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PaintDotNet.Quantization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
